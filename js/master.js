b87e1d8aed89f844a2743d49aaa5bd52a6976ff3
shalizsharafi <shalizoputh@gmail.com>
2026-05-29 21:56:33 -0700

    new sections added'

--- js/master.js
+++ js/master.js
@@ -15,4 +15,20 @@ window.addEventListener('scroll',()=>{
               }
        })
 })
-//card scroll effect
+//AboutFigurs scroll effect
+const aboutFigures = document.querySelectorAll('.aboutFigures')
+
+aboutFigures.forEach((val)=>{
+       val.classList.add(val.getAttribute('data-shaliz'))
+       val.parentElement.parentElement.parentElement.setAttribute('data-top',val.parentElement.parentElement.parentElement.offsetTop)
+})
+
+window.addEventListener('scroll',()=>{
+       let scrollT = window.scrollY +300
+       aboutFigures.forEach((val)=>{
+              if(scrollT > val.parentElement.parentElement.parentElement.getAttribute('data-top')){
+                     val.classList.remove(val.getAttribute('data-shaliz'))
+              }
+       })
+})
+
